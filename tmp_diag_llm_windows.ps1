1b8d1e1f81842dcbd9135c2ff7f849840110eba9
Shiyi <scheng1@upv.edu.es>
2026-04-21 19:27:03 +0200

    Elinacion

--- tmp_diag_llm_windows.ps1
+++
@@ -1,6 +0,0 @@
-Set-Location 'A:\Master-pycatan'
-$env:PYTHONPATH = 'A:\Master-pycatan;A:\Master-pycatan\PyCatan'
-$env:CATAN_LLM_PROVIDER = 'ollama'
-$env:CATAN_LLM_MODEL = 'qwen2.5:0.5b'
-$env:CATAN_OLLAMA_BASE_URL = 'http://127.0.0.1:11434'
-python A:\Master-pycatan\tmp_diag_llm_windows.py
